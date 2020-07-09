2abf22021fbe3469fbbca729de390d58319e8af2
James Verret <49586941+smnaief@users.noreply.github.com>
2020-07-09 18:59:01 +0600

    Add files via upload

---
+++ product-menus.php
@@ -0,0 +1,8 @@
+<div class="options">  
+    <a class="dropbtn" href="house.php">Apartments</a> 
+    <a class="dropbtn" href="furniture.php">Furnitures</a>
+    <a class="dropbtn" href="car.php">Cars</a>
+    <a class="dropbtn" href="bike.php">Bikes</a>
+    <a class="dropbtn" href="appliance.php">Appliances</a>
+    <a class="dropbtn" href="electronic.php">Electronics</a>
+</div>
